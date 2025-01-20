6c39703993d25ea3db1b9cdd1bc5571d3fe3c1b6
Manav <themanavgohil@gmail.com>
2025-01-20 10:18:50 +0530

    first commit

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,2 @@
 const JWT_SECRET ="manav"
-
 module.exports = JWT_SECRET
--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb+srv://manav25gohil:NBOFnjuXZ8XWPVHw@cluster0.7du3n.mongodb.net/')
-
 const userSchema = ({
     username: String,
     password: String,
     firstName: String,
     lastName: String
 })
-
 const accountsSchema = ({
     userId:{
         type: mongoose.Schema.Types.ObjectId,
